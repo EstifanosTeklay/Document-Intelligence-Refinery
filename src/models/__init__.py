19644e160b8f22900f8c476747bbd1a6999db949
estifanosteklay1@gmail.com <estifanosteklay1@gmail.com>
2026-03-04 22:14:53 +0300

    chore(models): export PageIndex, PageIndexNode, SectionEntity from models package

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,4 +2,5 @@
 from .document_profile import DocumentProfile, OriginType, LayoutComplexity, DomainHint, ExtractionCost
 from .extracted_document import ExtractedDocument, ExtractedTable, ExtractedFigure, TextBlock, BoundingBox
 from .ldu import LDU, ChunkType, ChunkRelationship
+from .pageindex import PageIndex, PageIndexNode, SectionEntity
 from .provenance import ProvenanceChain, SourceCitation
